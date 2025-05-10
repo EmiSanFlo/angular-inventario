09da706e5e4e67faacfe2ca6b656826022fecbf6
EmiSanFlo <a21100316@ceti.mx>
2025-05-09 22:30:55 -0600

    BOTON PAYPAL AGREGADO

--- src/app/components/carrito/carrito.component.ts
+++ src/app/components/carrito/carrito.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { CarritoService } from '../../services/carrito.service';
 import { Producto } from '../../models/producto';
 
+declare const paypal: any;
 @Component({
   selector: 'app-carrito',
   standalone:true,
@@ -16,7 +17,58 @@ export class CarritoComponent implements OnInit{
 
   constructor(private carritoService : CarritoService){}
     ngOnInit(){
-      this.carrito=this.carritoService.obtenerCarrito();
+      this.carrito = this.carritoService.obtenerCarrito();
+      console.log(this.carrito); // Verifica si hay productos en el carrito
+
+      this.loadPayPalScript().then(() => {
+      this.initPayPalButton();
+      }).catch(err => {
+      console.error('Error al cargar el SDK de PayPal:', err);
+      });
+    }
+
+    loadPayPalScript(): Promise<void> {
+      return new Promise((resolve, reject) => {
+        if ((window as any).paypal) {
+          resolve(); // Si PayPal ya está cargado, no lo cargues de nuevo
+          return;
+        }
+        const script = document.createElement('script');
+        //en client-id pegar el id que les sale en paypal developers. Se meten a dashboard y donde dice credenciales
+        //en carrito.component.html tambien cambien el client-id
+        script.src = 'https://www.paypal.com/sdk/js?client-id=ARbAyq0vx3C2eyiZuvUmTRQZEZutl_-t9HsDjsn87MexI9It4CEQPGLcVcQd_E2aV8b6xbJ8u5e7RIfI&currency=USD';
+        script.onload = () => resolve();
+        script.onerror = () => reject('No se pudo cargar el SDK de PayPal');
+        document.body.appendChild(script);
+      });
+    }
+
+    getTotal(): number {
+      return this.carrito.reduce((total, producto) => total + producto.precio * producto.cantidad, 0);
+    }
+  
+    initPayPalButton(): void {
+      paypal.Buttons({
+        createOrder: (data: any, actions: any) => {
+          return actions.order.create({
+            purchase_units: [{
+              amount: {
+                value: this.getTotal().toFixed(2) // Total del carrito
+              }
+            }]
+          });
+        },
+        onApprove: (data: any, actions: any) => {
+          return actions.order.capture().then((details: any) => {
+            alert(`Pago completado por ${details.payer.name.given_name}`);
+            this.generarXML(); // Generar el recibo XML después del pago
+            this.carrito = []; // Vaciar el carrito después del pago
+          });
+        },
+        onError: (err: any) => {
+          console.error('Error en el pago:', err);
+        }
+      }).render('#paypal-button-container');
     }
 
     eliminarProducto(index: number): void{
